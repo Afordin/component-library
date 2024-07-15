9017dd1e5372c5b83c86173be01eb97e5f5e0a7d
Jarrison Cano <me@jarrison.dev>
2024-07-15 18:15:25 -0500

    update Toaster.spec.tsx and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-/// <reference types="vitest" />
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import { defineConfig, UserConfig } from 'vite';
+import type { InlineConfig } from 'vitest';
 
-export default defineConfig({
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
+
+const vitestConfig: VitestConfigExport = {
   plugins: [react()],
   test: {
-    environment: "jsdom",
+    environment: 'jsdom',
     globals: true,
   },
-});
+};
+
+export default defineConfig(vitestConfig);
